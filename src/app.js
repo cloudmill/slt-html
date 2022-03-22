6364daf5abef9480a28422aae21647118335c2f9
Nikita <nikita.ableev@mail.ru>
2022-03-22 15:02:41 +0300

    Update main page

--- src/app.js
+++ src/app.js
@@ -2,3 +2,4 @@ import "./styles/app.scss";
 
 import "./scripts/swipers";
 import "./scripts/header";
+import "./scripts/loader";
---
+++ src/scripts/loader.js
@@ -0,0 +1,32 @@
+import AOS from 'aos';
+
+document.body.classList.add('body--hidden')
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector('.loader')
+
+  if (loader) {
+    const DELAY = 1000
+    const layers = document.querySelectorAll('.loader__layer')
+
+    layers.forEach((item, i) => {
+      setTimeout(() => {
+        item.classList.add('active')
+      }, DELAY * i);
+    })
+
+    setTimeout(() => {
+      loader.classList.add('loader--hidden')
+      document.body.classList.remove('body--hidden')
+      window.scrollTo(0, 0);
+      AOS.init({
+        once: true,
+        offset: 0,
+        duration: 1000,
+      });
+
+    }, DELAY * layers.length);
+  } else {
+    document.body.classList.remove('body--hidden')
+  }
+})
