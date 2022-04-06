9668914a3f7bf15fea654758425d410b40349ae1
szatolokin <staszatolokin28@gmail.com>
2022-04-06 15:35:56 +0300

    Update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-import "./styles/app.scss";
+import "./styles/app";
 
-import "./scripts/header";
-import "./scripts/loader";
-import "./scripts/accordion";
-import "./scripts/video-scroll";
-import "./scripts/parallax";
-import "./scripts/order-panel";
-import "./scripts/catalog-spoilers";
-import "./scripts/mobile-menu/MobileMenu";
-import "./scripts/search-tabs";
+import "./scripts/test";
