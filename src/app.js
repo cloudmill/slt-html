25299771bf025dd2032627b31f28ceedd9a95cf7
Nikita <nikita.ableev@mail.ru>
2022-04-07 16:42:34 +0300

    Update catalog modal

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import "./scripts/fancybox";
 import "./scripts/video-scroll";
 import "./scripts/parallax";
 import "./scripts/select";
+import "./scripts/CatalogModal";
 import "./scripts/main-slider";
 import "./scripts/about-slider";
 import "./scripts/order-panel";
