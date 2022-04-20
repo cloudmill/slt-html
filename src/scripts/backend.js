29566cacdb37922dc41f4d09ca8ee33f99af1ec7
Gercin <rockinpeople.rip@gmail.com>
2022-04-20 18:46:32 +0300

    update ignore & snippetImg & filterCheckbox

--- src/scripts/backend.js
+++ src/scripts/backend.js
@@ -1,6 +1,16 @@
 $(function () {
     filterChange();
+    filterCheckbox();
+    snippetImg();
 });
+
+function snippetImg() {
+    $(document).ready(function () {
+        let img = $(document).find('[data-type=snippet-img-hide]');
+
+        $(document).find('[data-type=snippet-img-show]').html(img);
+    });
+}
 
 function filterChange() {
     $(document).on("change", "[data-type=js-filter-change]", function (e) {
@@ -18,6 +28,49 @@ function filterChange() {
                 ajax: 1,
                 value: value,
             },
+            success: function (r) {
+                $(document).find('[data-type=items-container-full]').empty();
+                $(document).find('[data-type=items-container-full]').append($(r));
+            },
+            error: function (r) {
+                console.debug(r);
+            }
+        });
+    });
+}
+
+function filterCheckbox() {
+    $(document).on("click", "[data-type=js-filter-checkbox]", function (e) {
+        e.preventDefault();
+
+        let parent = $(this).parent("[data-type=filter-parent]"),
+            field = $(this).attr("code"),
+            val = $(this).attr("value"),
+            data = {};
+
+        console.log("filter checkbox ");
+        console.log(field + ' - ' + val);
+        /*
+        parent.find("[data-type=js-filter-checkbox]").each(function (index, element) {
+            console.log(index + ' - ' + element);
+            if (element.attr(':checked')); {
+                let field = element.attr("code"),
+                    val = element.attr("value");
+
+                data[field][index] = val;
+            }
+
+        });
+        */
+
+        data['ajax'] = 1;
+
+        console.log(data);
+
+        $.ajax({
+            method: "POST",
+            url: window.location.href,
+            data: data,
             success: function (r) {
                 $(document).find('[data-type=items-container-full]').empty();
                 $(document).find('[data-type=items-container-full]').append($(r));
