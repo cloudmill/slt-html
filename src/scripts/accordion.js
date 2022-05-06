1bbd4e09272c0b71a85b24d6a22005a6e5731d31
Nikita <nikita.ableev@mail.ru>
2022-05-06 14:36:40 +0300

    Update

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,13 +1,32 @@
 
+function accordionDropdown(e, root) {
+  const accordion = $(root)
+  const target = $(e.target)
+
+  if (target.closest('[data-accordion-button]').length) {
+    target.closest(accordion).toggleClass('active');
+    target.closest(accordion).find('[data-accordion-dropdown]').eq(0).slideToggle()
+  }
+}
+
 $(() => {
   if ($('[data-accordion]').length) {
     window.addEventListener('click', (e) => {
-      const accordion = $('[data-accordion]')
-      const target = $(e.target)
+      accordionDropdown(e, '[data-accordion]')
+    })
+  }
+})
 
-      if (target.closest('[data-accordion-button]').length) {
-        target.closest(accordion).toggleClass('active');
-        target.closest(accordion).find('[data-accordion-dropdown]').eq(0).slideToggle()
+$(() => {
+  if ($('[data-objects-accordion]')) {
+    window.addEventListener('click', (e) => {
+      accordionDropdown(e, '[data-objects-accordion]')
+
+      const target = $(e.target).closest('[data-objects-accordion]')
+
+      if (!target.length) {
+        $('[data-accordion-dropdown]').slideUp()
+        $('[data-objects-accordion]').removeClass('active')
       }
     })
   }
