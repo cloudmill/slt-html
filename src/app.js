f75c80f72b689756d9e99ba5b7abfaf5e1534915
Nikita <nikita.ableev@mail.ru>
2022-07-14 15:13:30 +0300

    Update

--- src/app.js
+++ src/app.js
@@ -32,3 +32,73 @@ import "./scripts/search-input";
 // import "./scripts/file-input";
 import "./scripts/Files";
 import "./scripts/calculator";
+// import "./scripts/calculators/CalculatorBig";
+import {CalculatorBig} from './scripts/calculators/CalculatorBig'
+
+$(() => {
+  const table = document.querySelector('.test-table')
+
+  if (table) {
+    const columns = table.querySelectorAll('.test-col')
+    const data = [
+      [20, 25, 32, 40, 50, 64, 75, 90, 110],
+      [3.4, 4.2, 5.4, 6.7, 8.3, 10.5, 12.5, 15, 18.3],
+      [6, 6, 6, 6, 6, 6, 6, 6, 6, ],
+      [20.3, 25.3, 32.3, 40.3, 50.4, 63.4, 75.5, 90.7, 110.8],
+      [3.8, 4.6, 5.7, 7.0, 8.9, 10.8, 12.8, 15.6, 19]
+    ]
+
+    let i = 0;
+
+    for (; i < data.length; i++) {
+      for (let j = 0; j < data[0].length; j++) {
+        const cell = createCell(data[i][j])
+        columns[i].append(cell)
+      }
+    }
+
+    for (; i < columns.length; i++) {
+      for (let j = 0; j < data[0].length; j++) {
+        let content
+        
+        if (i === 5) {
+          content = (data[3][j] - data[4][j]) / (2 * data[4][j])
+          content = Math.round(content * 100) / 100
+        } else if (i === 6) {
+          content = data[3][j] - 2 * data[4][j]
+          content = Math.round(content * 10) / 10
+        } else if (i === 7) {
+          const G = columns[6].querySelectorAll('.test-cell')[j+2].textContent
+          content = 3.14 / 4000 * +G * +G
+          content = Math.round(content * 1000) / 1000
+        } else if (i === 8) {
+          content = ""
+        } else {
+          const G = columns[6].querySelectorAll('.test-cell')[j+2].textContent
+          const J = columns[i].querySelectorAll('.test-cell')[1].textContent
+          content = +G / 1000 / +J
+          content = Math.round(content * 10) / 10
+        }
+
+        if (i === 21) {
+          const V = columns[i - 1].querySelectorAll('.test-cell')[j+2].textContent
+          content = Math.log(3.7 * +V) / Math.log(10)
+          content = Math.round(content * 1000) / 1000
+        }
+
+        const cell = createCell(content)
+        columns[i].append(cell)
+      }
+    }
+
+    function createCell(content) {
+      const cell = document.createElement('div')
+      cell.classList.add('test-cell')
+      cell.innerHTML = content
+
+      return cell;
+    }
+  }
+
+  new CalculatorBig()
+})
