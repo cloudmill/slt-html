6ba3f5408d03c3404c9fbf614b82b57003c32491
Nikita <nikita.ableev@mail.ru>
2022-07-26 18:54:58 +0300

    Update big calculator

--- src/app.js
+++ src/app.js
@@ -33,86 +33,70 @@ import "./scripts/search-input";
 import "./scripts/Files";
 import "./scripts/calculator";
 // import "./scripts/calculators/CalculatorBig";
-import {CalculatorBig} from './scripts/calculators/CalculatorBig'
+import { CalculatorBig } from "./scripts/calculators/CalculatorBig";
 // import {CalculatorMain} from './scripts/calculators/CalculatorMain'
 
 $(() => {
-  const table = document.querySelector('.test-table')
+  const tables = document.querySelectorAll("[data-calc-table]");
 
-  if (table) {
-    const columns = table.querySelectorAll('.test-col')
-    // const data = [
-    //   [20, 25, 32, 40, 50, 64, 75, 90, 110],
-    //   [3.4, 4.2, 5.4, 6.7, 8.3, 10.5, 12.5, 15, 18.3],
-    //   [6, 6, 6, 6, 6, 6, 6, 6, 6, ],
-    //   [20.3, 25.3, 32.3, 40.3, 50.4, 63.4, 75.5, 90.7, 110.8],
-    //   [3.8, 4.6, 5.7, 7.0, 8.9, 10.8, 12.8, 15.6, 19]
-    // ]
-    const data = [
-      [20, 25, 32, 40, 50, 64, 75, 90, 110],
-      [3.4, 4.2, 5.4, 6.7, 8.3, 10.5, 12.5, 15, 18.3],
-      [6, 6, 6, 6, 6, 6, 6, 6, 6, ],
-      [20.3, 25.3, 32.3, 40.4, 50.5, 63.5, 75.7, 90.8, 110.8],
-      [3.8, 4.6, 5.8, 7.1, 9.0, 10.7, 12.8, 16, 19]
-    ]
+  if (tables.length) {
+    tables.forEach((table) => {
+      const columns = table.querySelectorAll(".test-col");
+      const data = JSON.parse(table.getAttribute("data-calc-table"));
 
-    let i = 0;
-
-    for (; i < data.length; i++) {
-      for (let j = 0; j < data[0].length; j++) {
-        const cell = createCell(data[i][j])
-        columns[i].append(cell)
-      }
-    }
-
-    for (; i < columns.length; i++) {
-      for (let j = 0; j < data[0].length; j++) {
-        let content
-        
-        if (i === 5) {
-          content = (data[3][j] - data[4][j]) / (2 * data[4][j])
-          content = Math.round(content * 100) / 100
-        } else if (i === 6) {
-          content = data[3][j] - 2 * data[4][j]
-          // content = Math.round(content * 10) / 10
-        } else if (i === 7) {
-          const G = columns[6].querySelectorAll('.test-cell')[j+2].textContent
-          content = 3.14 / 4000 * +G * +G
-          // content = Math.round(content * 1000) / 1000
-        } else if (i === 8) {
-          content = ""
-        } else {
-          const G = columns[6].querySelectorAll('.test-cell')[j+2].textContent
-          const J = columns[i].querySelectorAll('.test-cell')[1].textContent
-          content = +G / 1000 / +J
-          // content = Math.round(content * 10) / 10
+      let i = 0;
+      for (; i < data.length; i++) {
+        for (let j = 0; j < data[0].length; j++) {
+          const cell = createCell(data[i][j], data[i][j]);
+          columns[i].append(cell);
         }
-
-        // if (i === 20) {
-        //   content = Math.round(content)
-        // }
-        if (i === 21) {
-          const V = columns[i - 1].querySelectorAll('.test-cell')[j+2].textContent
-          content = Math.log(3.7 * +V) / Math.log(10)
-          // content = Math.round(content * 100) / 100
-        }
-
-        const cell = createCell(content)
-        columns[i].append(cell)
       }
-    }
 
-    function createCell(content) {
-      const cell = document.createElement('div')
-      cell.classList.add('test-cell')
-      cell.innerHTML = content
+      for (; i < columns.length; i++) {
+        for (let j = 0; j < data[0].length; j++) {
+          let value;
 
-      return cell;
-    }
+          if (i === 5) {
+            value = (data[3][j] - data[4][j]) / (2 * data[4][j]);
+          } else if (i === 6) {
+            value = data[3][j] - 2 * data[4][j];
+          } else if (i === 7) {
+            const G =
+              columns[6].querySelectorAll(".test-cell")[j + 2].textContent;
+            value = (3.14 / 4000) * +G * +G;
+          } else if (i === 8) {
+            value = "";
+          } else {
+            const G =
+              columns[6].querySelectorAll(".test-cell")[j + 2].textContent;
+            const J = columns[i].querySelectorAll(".test-cell")[1].textContent;
+            value = +G / 1000 / +J;
+          }
 
-    new CalculatorBig()
+          if (i === 21) {
+            const V =
+              columns[i - 1].querySelectorAll(".test-cell")[j + 2].textContent;
+            value = Math.log(3.7 * +V) / Math.log(10);
+          }
+
+          const text = Math.round(value * 100) / 100;
+          const cell = createCell(value, text);
+          columns[i].append(cell);
+        }
+      }
+
+      function createCell(value, text) {
+        const cell = document.createElement("div");
+        cell.classList.add("test-cell");
+        cell.setAttribute("data-cell-value", value);
+        cell.innerHTML = text;
+
+        return cell;
+      }
+    });
+    new CalculatorBig();
   }
-})
+});
 
 // $(() => {
 //   const calculator = document.querySelector('[data-calc=calculatorMain]')
--- src/scripts/calculators/CalculatorBig.js
+++ src/scripts/calculators/CalculatorBig.js
@@ -1,123 +1,163 @@
 // калькулятор на странице calculator-5
 export class CalculatorBig {
   constructor() {
-    this.data = []
-    this.fields = []
+    this.data = [];
+    // this.data = {
+    //   SDR6: [],
+    //   SDR7: [],
+    //   SDR11: [],
+    // };
+    this.fields = [];
     this.formData = {
       rate: NaN,
-      temp: NaN
-    }
+      temp: NaN,
+    };
 
-    this.init()
+    this.init();
   }
 
   init() {
-    this.changeHandler()
-    this.collectData()
-    this.collectFields()
+    this.changeHandler();
+    this.collectData();
+    this.collectFields();
   }
 
   collectData() {
-    const table = document.querySelector('.test-table')
-    const columns = table.querySelectorAll('.test-col')
+    const tables = document.querySelectorAll(".test-table");
 
-    for (let i = 0; i < columns.length; i++) {
-      if (i === 8) {
-        continue;
+    tables.forEach((table) => {
+      const columns = table.querySelectorAll(".test-col");
+      const name = table.getAttribute("data-table-name");
+      const arr = [];
+      for (let i = 0; i < columns.length; i++) {
+        if (i === 8) {
+          continue;
+        }
+        const values = [];
+        for (let j = 2; j < 11; j++) {
+          const cellValue = columns[i]
+            .querySelectorAll(".test-cell")
+            [j].getAttribute("data-cell-value");
+          values.push(+cellValue);
+        }
+        arr.push(values);
       }
-      const arr = []
-      for (let j = 2; j < 11; j++) {
-        const cellValue = columns[i].querySelectorAll('.test-cell')[j].textContent
-        arr.push(+cellValue)
-      }
-      this.data.push(arr)
-    }
+      this.data.push(arr);
+    });
   }
 
   collectFields() {
-    const grid = document.querySelector('[data-table]')
-    const columns = grid.querySelectorAll('[data-table-col]')
+    const grid = document.querySelector("[data-table]");
+    const columns = grid.querySelectorAll("[data-table-col]");
 
     for (let i = 1; i < columns.length; i++) {
-      const arr = []
+      const arr = [];
       for (let j = 1; j < 10; j++) {
-        const cell = columns[i].querySelectorAll('[data-table-cell]')
-        arr.push(cell[j])
+        const cell = columns[i].querySelectorAll("[data-table-cell]");
+        arr.push(cell[j]);
       }
-      this.fields.push(arr)
+      this.fields.push(arr);
     }
   }
 
   changeHandler() {
-    const inputs = document.querySelectorAll('[data-calc-input]')
+    const inputs = document.querySelectorAll("[data-calc-input]");
 
-    inputs.forEach(item => {
-      const property = item.getAttribute('data-calc-input')
+    inputs.forEach((item) => {
+      const property = item.getAttribute("data-calc-input");
       item.onchange = () => {
-        this.formData[property] = +item.value
-        
-        this.calculate()
-      }
-    })
+        this.formData[property] = +item.value;
+
+        this.calculate();
+      };
+    });
   }
 
   calculate() {
-    for (let i = 0; i < 9; i++) {
-      this.getSpeed(i)
-      this.getLosses(i)
+    for (let x = 0; x < this.data.length; x++) {
+      const data = this.data[x];
 
-      this.setSpeed(i)
-      this.setLosses(i)
+      for (let i = 0; i < 9; i++) {
+        this.getSpeed(data, i);
+        this.getLosses(data, i);
+
+        this.setSpeed(x, i);
+        this.setLosses(x, i);
+      }
     }
   }
 
   // Удельные потери
-  getLosses(i) {
+  getLosses(data, i) {
     // Внутр. диаметр
-    const x = this.data[6][i]
-    const y = this.formData.temp
-    const logs = 1 + (
-      (Math.log((this.speed)*(x/1000/((1.78*Math.pow(10, -6))/(1+0.0337*y+0.000221*Math.pow(y, 2))))) / Math.log(10)) 
-      / 
-      (Math.log(500 * this.data[19][i]) / Math.log(10))
-    )
-    const asd = ((Math.log((this.speed)*(x/1000/((1.78*Math.pow(10, -6))/(1+0.0337*y+0.000221*Math.pow(y, 2))))) / Math.log(10)) - 1)
-    // console.log((Math.log(500 * this.data[19][i]) / Math.log(10)));
-    // console.log(Math.pow(0.5 / this.data[20][i] * ((logs) / 2 + (1.312*(2-(logs))* this.data[20][i]) / asd), 2)); //?
-    this.losses = 
-    Math.pow(0.5 / this.data[20][i] * ((logs) / 2 + (1.312*(2-(logs))* this.data[20][i]) / asd), 2) * 
-    Math.pow(this.speed, 2)/2/9.81/x * 1000
+    const x = data[6][i];
+    const y = this.formData.temp;
+    const logs =
+      1 +
+      Math.log(
+        this.speed *
+          (x /
+            1000 /
+            ((1.78 * Math.pow(10, -6)) /
+              (1 + 0.0337 * y + 0.000221 * Math.pow(y, 2))))
+      ) /
+        Math.log(10) /
+        (Math.log(500 * data[19][i]) / Math.log(10));
+    const part =
+      Math.log(
+        this.speed *
+          (x /
+            1000 /
+            ((1.78 * Math.pow(10, -6)) /
+              (1 + 0.0337 * y + 0.000221 * Math.pow(y, 2))))
+      ) /
+        Math.log(10) -
+      1;
+
+    this.losses =
+      ((Math.pow(
+        (0.5 / data[20][i]) *
+          (logs / 2 + (1.312 * (2 - logs) * data[20][i]) / part),
+        2
+      ) *
+        Math.pow(this.speed, 2)) /
+        2 /
+        9.81 /
+        x) *
+      1000;
   }
 
-  setLosses(i) {
+  setLosses(x, i) {
     if (this.losses) {
-      this.fields[5][i].innerHTML = this.losses.toFixed(5)
+      this.fields[x+3][i].innerHTML = this.losses.toFixed(5);
     }
   }
 
   // Скорости
-  getSpeed(i) {
+  getSpeed(data, i) {
     // Внутр. диаметр
-    const x = this.data[6][i]
-    this.speed = this.formData.rate / 1000 / (3.14*(Math.pow((x/1000), 2)/4))
+    const x = data[6][i];
+    this.speed =
+      this.formData.rate / 1000 / (3.14 * (Math.pow(x / 1000, 2) / 4));
   }
 
-  setSpeed(i) {
-    const cells = this.fields[2]
+  setSpeed(x, i) {
+    const cells = this.fields[x];
     if (this.speed) {
-      cells[i].innerHTML = this.speed.toFixed(4)
+      cells[i].innerHTML = this.speed.toFixed(4);
 
       if (this.speed > 0.5 && this.speed < 1 && this.losses) {
-        cells[i].classList.add('active')
-        this.fields[2+3][i].classList.add('active')
+        cells[i].classList.add("active");
+        this.fields[x + 3][i].classList.add("active");
       } else {
-        if (cells[i].classList.contains('active')) {
-          cells[i].classList.remove('active')
-          this.fields[2+3][i].classList.remove('active')
+        if (cells[i].classList.contains("active")) {
+          cells[i].classList.remove("active");
+          this.fields[x + 3][i].classList.remove("active");
         }
       }
     } else {
-      cells[i].innerHTML = '<div class="calculator-page__grid-title">null</div>'
+      cells[i].innerHTML =
+        '<div class="calculator-page__grid-title">null</div>';
     }
   }
 }
